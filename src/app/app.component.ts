dcd82c805c45b6b83d5ec1ef2cb15c60f5d906d0
Michel Buffa <michelbuffa@MacBook-Pro-de-Michel-2.local>
2024-09-04 15:07:17 +0200

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'assignment-app';
+  title = 'Première application Angular';
 }
